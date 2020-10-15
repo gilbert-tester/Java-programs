0ca8b5ace5de7710c1bd69a58ef4e7e0f472e6ee
gilert <gilbert.baidya@gmail.com>
2020-10-14 17:58:03 -0700

    add 1 - 10 numbers

---
+++ src/main/java/Programs/Sum_1_TO_10.java
@@ -0,0 +1,19 @@
+package Programs;
+// Print sum of all the numbers from 1 - 10
+public class Sum_1_TO_10 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for (int i = 1; i<=10; i++) {
+			
+			sum = sum + i;
+		}
+		
+		System.out.println("The sum is : "+ sum);
+				
+		
+	}
+
+}
