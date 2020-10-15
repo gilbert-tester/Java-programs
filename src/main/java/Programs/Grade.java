79c5a68c38fe39d0ee2d6e5de4b39dbae3edb8d3
gilbert <gilbert.baidya@gmail.com>
2020-10-15 11:43:57 -0700

    Blood Donate

--- src/main/java/Programs/Grade.java
+++ src/main/java/Programs/Grade.java
@@ -6,18 +6,36 @@ public class Grade {
 
 	public static void main(String[] args) {
 
-		int year;
+		int marks;
 
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter year : ");
-		year = sc.nextInt();
+		System.out.print("Enter marks : ");
+		marks = sc.nextInt();
 
-		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-
-			System.out.println(year + " : Leap Year");
+		if (marks <50) {
+			
+			System.out.println("Fail");
+			
+		}
+		
+		else if (marks >= 60 && marks < 70) {
+			
+			System.out.println("C Grade");
+		}
+		else if (marks >= 70 && marks < 80) {
+			
+			System.out.println("B Grade");
+		}
+		else if (marks >= 80 && marks < 90) {
+			
+			System.out.println("A Grade");
+		}
+		else if (marks >= 90 && marks < 100) {
+			
+			System.out.println("A+ Grade");
 		} else {
-
-			System.out.println(year + " : Common Year");
+			
+			System.out.println("Invalid");
 		}
 
 	}
